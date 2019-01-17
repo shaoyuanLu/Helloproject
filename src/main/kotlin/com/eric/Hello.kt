b69976d33a0cfb0271c57ac11cd0e40294a037d9
shaoyuanLu <shaoyuan812@gmail.com>
2019-01-17 17:13:27 +0800

    Initial commit

---
+++ src/main/kotlin/com/eric/Hello.kt
@@ -0,0 +1,12 @@
+package com.eric
+
+fun main(args: Array<String>) {
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+class Human {
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
