f3b7ca2cc9f98beaf7f76e1b2450aa85c452b94f
shaoyuanLu <shaoyuan812@gmail.com>
2019-01-18 09:53:40 +0800

    Kotlin variable,data type

--- src/main/kotlin/com/eric/Hello.kt
+++ src/main/kotlin/com/eric/Hello.kt
@@ -4,6 +4,9 @@ fun main(args: Array<String>) {
 //    Human().hello()
     val h = Human()
     h.hello()
+    var age=19
+    val name:String="Eric"
+    println(name+age)
 }
 class Human {
     fun hello() {
