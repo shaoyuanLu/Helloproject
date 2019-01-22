21fc1288b95edafa92f177e8ba6eed4a6be853ad
shaoyuanLu <shaoyuan812@gmail.com>
2019-01-22 14:39:44 +0800

    Kotlin variable,data type

--- src/main/java/com/eric/Hello.java
+++ src/main/java/com/eric/Hello.java
@@ -2,8 +2,15 @@ package com.eric;
 
 public class Hello {
     public static void main(String[] args) {
-//        System.out.println("Hello world");
-        Person p=new Person();
-        p.Hello();
+        Person p=new Person("Eric",66.5f,1.7f);
+        //p.Hello();
+       /* p.weight=66.5f;
+        p.height=1.7f;
+        p.name="Eric";*/
+        System.out.println(p.name+p.bmi());
+        char c='A';
+        System.out.println(c>'a');
+
+
     }
 }
--- src/main/kotlin/com/eric/Hello.kt
+++ src/main/kotlin/com/eric/Hello.kt
@@ -2,13 +2,26 @@ package com.eric
 
 fun main(args: Array<String>) {
 //    Human().hello()
-    val h = Human()
-    h.hello()
+    val h = Human(weight =  66.5f, height = 1.7f)
+    h.bmi()
+    println(h.bmi())
+    val c:Char='A'
+    println(c.toInt()>60)
+
+  /*  h.hello()
     var age=19
-    val name:String="Eric"
-    println(name+age)
+    var name="Eric"
+    println(name+age)*/
 }
-class Human {
+class Human(var weight:Float,var height:Float,var name:String="") {
+    init {
+        println("test $weight$height")
+    }
+    fun bmi():Float{
+        val bmi=weight/(height*height)
+        return bmi
+
+    }
     fun hello() {
         println("Hello kotlin")
     }
